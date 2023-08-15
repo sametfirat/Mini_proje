969ada575cbac218bc3049b0ed7144b9f4c7e494
samet <samet@CASPER>
2023-08-15 23:27:31 +0300

    Proje dosyası ekle.

---
+++ Mini_proje/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_proje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            musteri M1 = new musteri();
+            M1.Name = "samet";
+            M1.Id = 1;
+            M1.Sorname = "firat";
+            M1.Password ="password";
+            M1.Email = "samet.firat";
+            M1._kulancıAdı = "sametfırat";
+            musteri.ekle(M1);
+
+            musteri M2 = new musteri();
+            M2.Name = "ali";
+            M2.Id = 1;
+            M2.Sorname = "firat";
+            M2.Password = "password";
+            M2.Email = "fatih.firat";
+            M2._kulancıAdı = "fatih";
+            musteri.ekle(M2);
+
+            musteri M3 = new musteri();
+            M3.Name = "samet";
+            M3.Id = 1;
+            M3.Sorname = "firat";
+            M3.Password = "password";
+            M3.Email = "samet.firat";
+            M3._kulancıAdı = "sametfırat";
+            musteri.ekle(M3);
+
+        }
+    }
+}
---
+++ Mini_proje/personel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_proje
+{
+    internal class musteri
+    {
+        static ArrayList dataBase;
+        static musteri()
+        {
+            dataBase = new ArrayList();
+        }
+        
+        public int Id ;
+        public string Name ;
+        public string Sorname;
+        public string Email;
+        public string Password ;
+        private string Kulanıcıadı;
+        
+        public string _kulancıAdı
+        {
+            get
+            {
+                return this.Kulanıcıadı;
+            }
+            set
+            {
+                bool kontrol = kulanıcıAdıSorgulama(value);
+
+                if (kontrol == true) { 
+                    Console.WriteLine("kulanıcı adı bulnmaktadır");
+                    this.Kulanıcıadı = string.Empty;
+                }
+                else
+                {
+                    this.Kulanıcıadı = value;
+                }
+                
+
+            }
+        }
+            static bool kulanıcıAdıSorgulama(string _kulanıcıadı)
+        {
+            bool flag = false;
+            for (int i = 0 ; i<dataBase.Count ; i++)
+            {
+                musteri temp = (musteri)dataBase[i];
+                if (temp.Kulanıcıadı== _kulanıcıadı)
+                {
+                    flag = true;
+                    break;
+                }
+                
+            }
+            return flag;
+        }
+        static bool kulanıcıMailSorgula(string email)
+        {
+            bool flag = false;
+            for(int i=0;  i<dataBase.Count ; i++)
+            {
+                musteri cont = (musteri)dataBase[i];
+                if (cont.Email== email)
+                {
+                    flag = true;
+                }
+            }
+            return flag;
+        }
+        public static void ekle (musteri M)
+        {
+            if (M != null && !string.IsNullOrEmpty(M.Kulanıcıadı) && !string.IsNullOrEmpty(M.Email))
+            {
+                
+                bool emailSorgula = kulanıcıAdıSorgulama(M.Email);
+
+                if(emailSorgula == true)
+                {
+                    Console.WriteLine("sistemde kayıtlı");
+                } 
+
+            else
+                {
+                    dataBase.Add(M);
+                    Console.WriteLine("yeni kayıt eklendi");
+                }
+            }
+        }
+
+    }
+}
